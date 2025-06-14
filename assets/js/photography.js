6369184a7ff0bb824329a3a12c4ceb808e7102e0
Ferrixio <samu9.ferri@gmail.com>
2025-06-14 15:37:48 +0200

    Fast load photography

--- assets/js/photography.js
+++ assets/js/photography.js
@@ -1,7 +1,10 @@
 const imageFolder = './assets/css/photo/';
 const container = document.getElementById('photo-container');
 
-fetch('https://ferrixio.github.io/assets/js/photo.json')
+const scale = 0.1;
+
+// fetch('https://ferrixio.github.io/assets/js/photo.json')
+fetch('./assets/js/photo.json')
     .then(response => response.json())
     .then(images => {
         images.forEach(image => {
@@ -14,20 +17,30 @@ fetch('https://ferrixio.github.io/assets/js/photo.json')
             link.href = imageFolder + image.file;
             link.target = '_blank';
 
-            // Create <img> element
-            const img = document.createElement('img');
-            img.src = imageFolder + image.file;
-            img.style.objectPosition = `${image.position}`;
+            const canvas = document.createElement('canvas');
 
             // Create <figcaption> with two lines
             const caption = document.createElement('figcaption');
             caption.innerHTML = `${image.text}<br>${image.date}`;
 
+            // Create <img> element
+            const img = document.createElement('img');
+            img.src = imageFolder + image.file;
+            img.style.objectPosition = `${image.position}`;
+
             // Build the structure
-            link.appendChild(img);
+            link.appendChild(canvas);
             link.appendChild(caption);
             wrapper.appendChild(link);
             container.appendChild(wrapper);
+
+            // substitute it with canvas and resize it to 10% of original sizes
+            img.onload = () => {
+                canvas.width = img.naturalWidth * scale;
+                canvas.height = img.naturalHeight * scale;
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            }
         });
     })
     .catch(err => console.error('Error loading images: ', err));
